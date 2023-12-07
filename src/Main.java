aa5e31b17836406e2a79961f5b4ddf4a0ff1babb
vibesx <lucian.paslariu@gmail.com>
2023-12-07 23:27:30 +0200

    finished problem 1 part 2

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         Problem1 problem1 = new Problem1();
-        problem1.solve();
+        problem1.solve_part2();
 //        ReadFileUtil.test();
 //        System.out.println(new File(".").getAbsolutePath());
 //        System.out.println(System.getProperty("user.dir"));
--- src/Year2023/Problem1.java
+++ src/Year2023/Problem1.java
@@ -3,14 +3,34 @@ package Year2023;
 import Year2023.util.ReadFileUtil;
 
 import java.util.List;
+import java.util.Map;
 
 public class Problem1 {
+
+    public static final Map<String, Integer> numbers = Map.of("one", 1,
+            "two", 2,
+            "three", 3,
+            "four", 4,
+            "five", 5,
+            "six", 6,
+            "seven" ,7,
+            "eight", 8,
+            "nine", 9);
+
     List<String> input = ReadFileUtil.fileToStringList("src/Year2023/inputs/Problem1.txt");
 
     public void solve() {
         int sum = 0;
         for(String value : input) {
             sum += getCalibrationValue(value);
+        }
+        System.out.println(sum);
+    }
+
+    public void solve_part2() {
+        int sum = 0;
+        for(String value : input) {
+            sum += getCalibrationValue_part2(value);
         }
         System.out.println(sum);
     }
@@ -31,6 +51,42 @@ public class Problem1 {
             }
         }
         return result;
+    }
+
+    private int getCalibrationValue_part2(String value) {
+        int minIndex = Integer.MAX_VALUE;
+        int maxIndex = -1;
+        int leftDigit = 0;
+        int rightDigit = 0;
+        for(String number : numbers.keySet()) {
+            int firstIndexOfNumber = value.indexOf(number);
+            int lastIndexOfNumber = value.lastIndexOf(number);
+            if(firstIndexOfNumber >= 0) {
+                if(firstIndexOfNumber < minIndex) {
+                    minIndex = firstIndexOfNumber;
+                    leftDigit = numbers.get(number);
+                }
+                if(lastIndexOfNumber > maxIndex) {
+                    maxIndex = lastIndexOfNumber;
+                    rightDigit = numbers.get(number);
+                }
+            }
+        }
+        for(Integer number : numbers.values()) {
+            int indexOfNumber = value.indexOf(String.valueOf(number));
+            int lastIndexOfNumber = value.lastIndexOf(String.valueOf(number));
+            if(indexOfNumber >= 0) {
+                if(indexOfNumber < minIndex) {
+                    minIndex = indexOfNumber;
+                    leftDigit = number;
+                }
+                if(lastIndexOfNumber > maxIndex) {
+                    maxIndex = lastIndexOfNumber;
+                    rightDigit = number;
+                }
+            }
+        }
+        return 10 * leftDigit + rightDigit;
     }
 
 
